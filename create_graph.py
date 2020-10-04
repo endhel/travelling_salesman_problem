7e1499170d3a5a9743b9d88bdab2ca36b57aaf4e
endhel <endheltog@gmail.com>
2020-10-04 12:11:16 -0300

    updated create_graph

--- create_graph.py
+++ create_graph.py
@@ -5,7 +5,7 @@ def create_graph(num_vertices, min_weight, max_weight):
     graph = [[0 for _ in range(num_vertices)] for _ in range(num_vertices)]
 
     for i in range(num_vertices):
-        for j in range(num_vertices):
+        for j in range(num_vertices - 1):
             if i == j:
                 continue
             if graph[i][j] == 0:
