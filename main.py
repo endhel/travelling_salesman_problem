564c2fd26944ea81d5abbfee2108350645c8935e
endhel <endheltog@gmail.com>
2020-10-04 14:51:27 -0300

    Finished Project

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 from create_graph import create_graph
 from time import time
 from nearest_neighbors import nearest_neighbors
-
-
-def rec_cost(g, p):
-    cost = 0
-    for j in range(len(p)):
-        if j == len(g):
-            break
-        cost += g[p[j]][p[j + 1]]
-    return cost
+from brute_force import brute_force
+from recover_cost import rec_cost
 
 
 print('Grafo:')
 graph = create_graph(5, 1, 9)
+
 for i in range(len(graph)):
     print(graph[i])
 print()
@@ -32,7 +26,8 @@ if option == 1:
     print(f'Custo do Caminho: {rec_cost(graph, path)}')
     print(f'Tempo: {fim - inicio}')
 if option == 2:
-    print('Caminho: ')
-    print(f'Custo do Caminho:')
+    path = brute_force(graph)
     fim = time()
+    print(f'Caminho: {path}')
+    print(f'Custo do Caminho: {rec_cost(graph, path)}')
     print(f'Tempo: {fim - inicio}')
