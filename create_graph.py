78a2dee48b2175cb9d3e7a980f98e9e707d0c1d3
endhel <endheltog@gmail.com>
2020-10-03 21:40:15 -0300

    Create create_graph.py

---
+++ create_graph.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+def create_graph(num_vertices, min_weight, max_weight):
+    graph = [[0 for _ in range(num_vertices)] for _ in range(num_vertices)]
+
+    for i in range(num_vertices):
+        for j in range(num_vertices):
+            if i == j:
+                continue
+            if graph[i][j] == 0:
+                weight = randint(min_weight, max_weight)
+                graph[i][j] = graph[j][i] = weight
+    return graph
